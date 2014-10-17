289a0482dc4bde2a98e32b0dc579f73bcd34ba20
saasbook <saasbook@saasbook.(none)>
2014-10-16 21:50:46 -0300

    Updated

---
+++ myrottenpotatoes/db/migrate/20141016221501_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration
+  def up
+create_table 'movies' do |t|
+t.string 'title'
+t.string 'rating'
+t.text 'description'
+t.datetime 'release_date'
+#Add fields that let Rails automatically keep track of movies
+t.timestamps
+  end
+end
+
+  def down
+drop_table 'movies'
+  end
+end
